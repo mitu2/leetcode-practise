d334b8ba5bf48512b3fbf542b24eab0f98a5d1be
chenmoand <chenmoand@outlook.com>
2022-03-29 19:38:28 +0800

    modify ZigZagConversion_6

--- src/main/java/runstatic/leetcode/annotation/Wrong.java
+++ src/main/java/runstatic/leetcode/annotation/Wrong.java
@@ -8,4 +8,5 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
 public @interface Wrong {
+    String value() default "";
 }
--- src/main/java/runstatic/leetcode/i1_10/ZigZagConversion_6.java
+++ src/main/java/runstatic/leetcode/i1_10/ZigZagConversion_6.java
@@ -1,33 +1,59 @@
 package runstatic.leetcode.i1_10;
 
-import runstatic.leetcode.annotation.TODO;
+import runstatic.leetcode.annotation.Wrong;
 
 /**
  * @author chenmoand
  */
 public class ZigZagConversion_6 {
 
-    @TODO("未完成")
+    @Wrong("错误的理解")
     public String convert(String s, int numRows) {
         char[] chars = s.toCharArray();
-        int cenChr = numRows - 2;
         StringBuilder sb = new StringBuilder();
-//        int indexMaxLen = chars.length - 1;
 
-        int sm = numRows + 1;
+        int vNum = (2 * numRows - 2);
+        int maxRow = chars.length / vNum;
+        int inc = chars.length % vNum;
+        boolean isEven = numRows % 2 == 0;
+        System.out.println("vNum = " + vNum + " inc = " + inc + " maxRow = " + maxRow);
+        // (numRows - 2) + (numRows - 1)
+        int sm = 2 * numRows - 3;
         for (int i = 0; i < numRows; ++i) {
-            int appendOffset = i + numRows;
+            // int appendOffset = i + numRows;
             boolean isNextSpecial = false;
             boolean isMid = i != 0 && i != (numRows - 1);
-            int leftSpecialOffset = sm -  getOffset(i);
+            int leftSpecialOffset = sm - getOffset(i);
             int rightSpecialOffset = sm - leftSpecialOffset - 1;
-            // TODO 优化输出
-            for (int index = i; index < chars.length; index += appendOffset) {
-                sb.append(chars[index]);
-                if(index + appendOffset >= chars.length && i != numRows - 1) {
+            int curColMaxRow = maxRow;
+            if (i == numRows - 1 || i == 0) {
+                curColMaxRow -= (isEven ? numRows - i + 1 : i) - inc >= 0 ? 1 : 0;
+            } else {
+                curColMaxRow = maxRow + (maxRow - 1) + ((isEven ? numRows - i + 1 : i) - inc >= 0 ? 0 : 1);
+            }
+            for (int curRow = 0; curRow <= curColMaxRow; ++curRow) {
+                // System.out.println("i = " + i + "  curRow = " + curRow);
+
+                if (curRow == 0) {
+                    sb.append(chars[i]);
+                } else if (isMid) {
+                    if (isNextSpecial) {
+                        sb.append(chars[(numRows - i - 1) + curRow * (numRows - 2) + curRow]);
+                    } else {
+                        sb.append(chars[i + curRow * (numRows - 2) + curRow]);
+                    }
+                } else {
+                    // System.out.println("i = " + i + "  curRow = " + curRow);
+                    sb.append(chars[i + curRow * (numRows + 1)]);
+                }
+
+
+                // sb.append('-');
+                if (curRow == curColMaxRow) {
                     sb.append('\n');
                     continue;
                 }
+                // index += appendOffset;
                 if (!isMid) {
                     sb.append(getBlank(sm));
                     continue;
@@ -45,11 +71,10 @@ public class ZigZagConversion_6 {
 
         return sb.toString();
     }
+
     public int getOffset(int i) {
-        if(i == 1) {
-            return 2;
-        }
-        return i + 2;
+        // i + i
+        return i << 1;
     }
 
     public StringBuilder getBlank(int num) {
@@ -62,7 +87,11 @@ public class ZigZagConversion_6 {
 
     public static void main(String[] args) {
         ZigZagConversion_6 instance = new ZigZagConversion_6();
-        System.out.println(instance.convert("ABCDEFGADAHGGFFYFYUFYFFGJHFGJHFJHFJGHUKGUGIUGG", 4));
+        /**
+         * A 00   0
+         * E 01 3 4
+         */
+        System.out.println(instance.convert("PAYPALISHIRING", 3));
     }
 
 }
