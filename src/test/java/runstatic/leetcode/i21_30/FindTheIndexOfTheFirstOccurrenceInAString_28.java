348b8b3a517a2e01e7a5f35dfc1b7b1536ee25f6
chenmoand <chenmoand@outlook.com>
2022-12-02 15:01:59 +0800

    add 28

---
+++ src/test/java/runstatic/leetcode/i21_30/FindTheIndexOfTheFirstOccurrenceInAString_28.java
@@ -0,0 +1,41 @@
+package runstatic.leetcode.i21_30;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author chenmoand
+ */
+public class FindTheIndexOfTheFirstOccurrenceInAString_28 {
+    public int strStr(String haystack, String needle) {
+        // note: String#indexOf(needle) 最快捷的实现方式
+        // return haystack.indexOf(needle);
+        if (haystack.length() < needle.length()) {
+            return -1;
+        }
+        if(haystack.equals(needle)) {
+            return 0;
+        }
+        char[] haystackChars = haystack.toCharArray();
+        char[] needleChars = needle.toCharArray();
+        int index = 0;
+        int spec = needleChars.length;
+        while (index <= haystackChars.length - spec) {
+            if (Arrays.equals(haystackChars, index, index + spec, needleChars, 0, spec)) {
+                return index;
+            }
+            ++index;
+        }
+        return -1;
+    }
+
+    @Test
+    public void testIndexOf() {
+        Assert.assertEquals(2, strStr("abccc", "cc"));
+        Assert.assertEquals(0, strStr("a", "a"));
+        Assert.assertEquals(2, strStr("abc", "c"));
+
+    }
+}
